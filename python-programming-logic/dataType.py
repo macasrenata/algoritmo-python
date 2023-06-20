8a89ab4d2ba1e873151797842d919ad81d958b2a
Renata Maçãs <renata.macas@gmail.com>
2023-06-20 15:02:45 -0300

    add logic

---
+++ python-programming-logic/dataType.py
@@ -0,0 +1,101 @@
+#Tipo de dados
+
+# string (texto)
+"introdução ao worpress"
+
+# integer (número inteiro)
+10
+
+# float (número decimal)
+10.5
+
+# boolean (verdadeiro ou falso)
+True
+
+# list (lista)
+[10, 20, 30, 40, 50]
+
+# tuple (tupla)
+(10, 20, 30, 40, 50)
+
+
+
+# Operações matemáticas
+
+# soma
+10 + 10
+
+# subtração
+10 - 5
+
+# multiplicação
+10 * 10
+
+# divisão
+10 / 2
+
+# exponenciação
+10 ** 2
+
+# módulo
+10 % 2
+
+# divisão inteira
+10 // 2
+
+
+#Variaveis
+
+#string 
+nome = "introdução ao worpress"
+
+# integer(integer, int)
+duração = 85
+
+# float (float)
+preço = 10.5
+
+# boolean (bool)
+ativo = True
+
+# list (list)
+tags = ["wordpress", "cms", "php"]
+
+# tuple (tuple)
+tags = ("wordpress", "cms", "php")
+
+# dictionary (dict)
+produto = {
+    "nome": "Curso de Wordpress",
+    "duração": 85,
+    "preço": 10.5,
+    "ativo": True,
+    "tags": ["wordpress", "cms", "php"]
+}
+
+# Operadores de atribuição
+
+# atribuição
+x = 10
+
+# atribuição com soma
+x += 10
+
+# atribuição com subtração
+x -= 10
+
+# atribuição com multiplicação
+x *= 10
+
+# atribuição com divisão
+x /= 10
+
+# atribuição com exponenciação
+x **= 10
+
+# atribuição com módulo
+x %= 10
+
+# atribuição com divisão inteira
+x //= 10
+
