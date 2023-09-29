1da47de631de93bf3203ccad71c0d3da82837592
Renata Maçãs <renata.macas@gmail.com>
2023-09-28 21:04:30 -0300

    capitulo 1 python

---
+++ start-python/print.py
@@ -0,0 +1,33 @@
+# Autor: Renata Maçãs
+
+# saida de dados
+
+print("string")
+
+# saida: string
+
+print("string", "string", sep=",")
+
+# saida: string,string
+# sep: separador
+
+print("string", "string", sep=",", end="\n")
+
+# saida: string,string
+#end: final da linha (quebra de linha)
+
+
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+# saida: Python_é_fantástico!
+
+
+dia = 15
+mes = 10
+ano = 2015
+print(dia, mes, ano, sep="/")
+
+# saida: 15/10/2015
