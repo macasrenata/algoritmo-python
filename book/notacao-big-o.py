24eae21fd8c83c63d5430b4ee4d3976417992158
macasrenata <renata.macas@gmail.com>
2024-09-30 10:00:12 -0300

    update bigO

--- book/notacao-big-o.py
+++ book/notacao-big-o.py
@@ -2,3 +2,38 @@
 # (n) = numero de operações que o algoritmo precisa fazer
 
 # tempo de execução crescem com velocidades diferentes
+
+# notação big o estabelece o tempo de execução de um algoritmo
+
+# pesquisa simples é igual a O(n) pq percorrera todos os elementos, por exemplo de uma lista telefonica, para encontrar um nome de uma 
+# pessoa vc inicia na letra A e não pelo inicio da primeira letra do nome da pessoa
+
+# exemplos de notação BIg O do mais rapido para o mais lento no tempo de execução:
+
+# O(log n) - logaritmo = pesquisa binaria
+
+# O(n) - linear = pesquisa simples 
+
+# O(n*log n) - quicksort = pesquisa rapida
+
+# O(n²) - quadrático = bubble sort / algoritimo lento de ordenação
+
+# O(n!) - fatorial = algoritimo muito lento , exemplo do caixeiro viajante
+
+# a rapidez de um algoritimo nao e medida em segundos, mas sim em crescimento de operações
+
+# O quanto rapidamente o tempo de execução de um algoritimo aumenta conforme o tamanho da entrada aumenta
+
+# o tempo de execução de um algoritimo é expresso na notação Big O
+
+# O(log n) é mais rapido que O(n), O(log n) fica mais rapido conforme o tamanho da entrada aumenta (lista)
+
+# pesquisa binaria é mais rapida que pesquisa simples
+
+# O(log n) é mais rapido que O(n), e O(log n) fica mais rapido conforme o tamanho da entrada aumenta (lista)
+
+# a rapidez de um algoritimo nao e medida em segundos, mas sim em crescimento de operações
+
+# o tempo de execução de um algoritimo é medido por meio de seu crescimento
+
+# o tempo de execução de um algoritimo é expresso na notação Big O
