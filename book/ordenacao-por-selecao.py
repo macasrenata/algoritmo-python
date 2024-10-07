ec6014348b8bbf64063e5da8fe8fc41d0910c967
macasrenata <renata.macas@gmail.com>
2024-10-07 10:57:25 -0300

    update

---
+++ book/ordenacao-por-selecao.py
@@ -0,0 +1,6 @@
+# arrays e listas encadeadas 
+
+# listas encadeadas resolvem o problema de adição d eitens no meio da lista, pois não é necessario mover todos os itens para adicionar um novo item
+# os itens podem estar em qualquer lugar da memoria
+
+#arrays 
