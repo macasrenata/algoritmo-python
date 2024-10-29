f9c61895753a565244bf9089d46e66a094c657bf
macasrenata <renata.macas@gmail.com>
2024-10-29 15:11:32 -0300

    add recursao

--- book/ordenacao-por-selecao.py
+++ book/ordenacao-por-selecao.py
@@ -4,3 +4,26 @@
 # os itens podem estar em qualquer lugar da memoria
 
 #arrays 
+# função para encontrar o menor elemento do array
+
+def findSmallest(arr):
+    # armazena o menor valor
+    smallest = arr[0]
+    # armazena o indice do menor valor
+    smallest_index = 0
+    for i in range(1, len(arr)): # percorre o array
+        if arr[i] < smallest: # se o elemento atual for menor que o menor elemento
+            smallest = arr[i] # atualiza o menor elemento
+            smallest_index = i # atualiza o indice do menor elemento
+    return smallest_index # retorna o indice do menor elemento
+
+# função para ordenar um array
+def selectionSort(arr):
+    newArr = [] # cria um novo array
+    for i in range(len(arr)): # percorre o array
+        # encontra o menor elemento do array e adiciona ao novo array
+        smallest = findSmallest(arr) # encontra o menor elemento do array
+        newArr.append(arr.pop(smallest)) # remove o menor elemento do array e adiciona ao novo array
+    return newArr # retorna o novo array
+
+print(selectionSort([5, 3, 6, 2, 10])) # [2, 3, 5, 6, 10]
---
+++ book/recursao.py
@@ -0,0 +1,5 @@
+# recursão
+# é uma técnica de programação onde uma função chama a si mesma
+# caso base e caso recursivo
+
+# dividir para conquistar
