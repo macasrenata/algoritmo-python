1c57b4ac1be8371ce93d8b056963eea897259154
macasrenata <renata.macas@gmail.com>
2024-11-05 17:46:09 -0300

    add recursão

--- book/ordenacao-por-selecao.py
+++ book/ordenacao-por-selecao.py
@@ -27,3 +27,4 @@ def selectionSort(arr):
     return newArr # retorna o novo array
 
 print(selectionSort([5, 3, 6, 2, 10])) # [2, 3, 5, 6, 10]
+
--- book/recursao.py
+++ book/recursao.py
@@ -3,3 +3,24 @@
 # caso base e caso recursivo
 
 # dividir para conquistar
+
+# Recursão pode ser feita com um loop - while até finalizar (arrumando em pilhas)
+# ou com recursão de cauda - a chamada recursiva é a última instrução da função
+
+# Ou seja, recursão é quando a função chama a si mesma
+
+# pseudocodigo:
+
+def procure_chave(caixa):
+    for item in caixa:
+        if item.is_a_box():
+            procure_chave(item)
+        elif item.is_a_key():
+            print("Achei a chave!")
+
+# A função procure_chave() é recursiva, pois chama a si mesma
+
+# Recursão é usada para deixar a resposta mais clara e mais simples
+
+# " Os loops podem melhorar o desempenho do seu programa. A recursão melhora o desempenho do programador. Escolha o que for mais importante para a sua situação. " - Leigh Caldwell
+
